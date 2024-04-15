223f7062063a309f3d6110566624f959ba1f78ca
Matthew Molloy <whamtet@gmail.com>
2024-04-16 05:29:21 +0900

    added tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./target/generated-sources/tailwind/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
