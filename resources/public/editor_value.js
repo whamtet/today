b0c7d99f681cc70ee8570d14f6c0726b4b8fcaae
Matthew Molloy <whamtet@gmail.com>
2024-05-05 11:24:08 +0900

    small bugfix

--- resources/public/editor_value.js
+++ resources/public/editor_value.js
@@ -6,13 +6,18 @@ const getEditorMovements = () => {
     let reference = editor.querySelector('.reference');
     while (reference) {
 
-        const oldOffset = reference.dataset.offset;
-        reference.innerHTML = marker;
-        const newOffset = editor.innerText.indexOf(marker);
-        movements[oldOffset] = newOffset;
+        if (!reference.innerHTML.trim()) {
+            reference.parentNode.removeChild(reference);
+            reference = editor.querySelector('.reference');
+        } else {
+            const oldOffset = reference.dataset.offset;
+            reference.innerHTML = marker;
+            const newOffset = editor.innerText.indexOf(marker);
+            movements[oldOffset] = newOffset;
 
-        reference.parentNode.removeChild(reference);
-        reference = editor.querySelector('.reference');
+            reference.parentNode.removeChild(reference);
+            reference = editor.querySelector('.reference');
+        }
     }
     return movements;
 }
