8f81c792dd7a9a9ea17abd39a41377ae4a82bcde
Matthew Molloy <whamtet@gmail.com>
2024-05-16 15:44:03 +0900

    return migration-pending? boolean

--- src/clj/diligence/today/web/controllers/question/migrate.clj
+++ src/clj/diligence/today/web/controllers/question/migrate.clj
@@ -4,18 +4,32 @@
       [diligence.today.web.controllers.file :as file]
       [diligence.today.web.controllers.question :as question]))
 
-;; fun times!
-
-(defn- migrate-editor [req]
-  (fn [{:keys [file_id page line] :as m}]
-    (if-let [new-page (file/whole-page req file_id page)]
+(defn- migrate-editor-value [req file_id m]
+  (if (-> m :file_id (= file_id))
+    (if-let [new-page (file/whole-page req file_id (:page m))]
       (assoc m :page new-page)
-      (if-let [[new-page new-line] (file/move-line req file_id page line)]
+      (if-let [[new-page new-line] (file/move-line req file_id (:page m) (:line m))]
         (assoc m :page new-page :line new-line) ;; might need to put migration-pending? here too
-        (assoc m :migration-pending? true)))))
+        (assoc m :migration-pending? true)))
+    m))
+
+(defn migrate-questions [req project_id file_id]
+  (let [migrated (map
+                  (fn [question]
+                    (update question
+                            :editor
+                            (fn [editor]
+                              (util/map-vals #(migrate-editor-value req file_id %) editor))))
+                  (question/get-questions-file req project_id file_id))]
+    (doseq [{:keys [question_id editor]} migrated]
+      (question/set-editor req question_id editor))
+    (some
+     #(->> % :editor vals (some :migration-pending?))
+     migrated)))
+
 
 (defn mark-migrated [req project_id file_id]
   (doseq [{:keys [question_id editor]} (question/get-questions-file req project_id file_id)]
     (->> editor
          (util/map-vals (migrate-editor req))
-         (question/set-editor req project_id))))
+         (question/set-editor req question_id))))
